c4d71b5704d36fcf23148d224e527c7826616847
bryzgin <k.bryzgin@yandex.ru>
2026-09-04 13:15:59 +0700

    [feat/triangulation] create triangulation.h interface

--- include/triangulation.h
+++ include/triangulation.h
@@ -12,15 +12,18 @@
  * @struct Circle
  * @brief additional structure for the circle
  */
+typedef struct {
+    float center_x;
+    float center_y;
+    float radius
+} Circle;
 
-
-
-
-
-
-
-
+/**
+ * @brief build a Delaunay triangulation
+ * @param dtm DTM pointer
+ * @return int 1 in case of success, 0 in case of a memory allocation error
+ */
+int triangulate_delaunay(DTM* dtm);
 
 #endif
  
-
