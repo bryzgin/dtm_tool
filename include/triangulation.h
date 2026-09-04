f99d873d1898e2b5631bc34e3aeb1580a7cdcbd2
bryzgin <k.bryzgin@yandex.ru>
2026-09-04 13:07:43 +0700

    [feat/triangulation] triangulation.h wip

---
+++ include/triangulation.h
@@ -0,0 +1,26 @@
+/**
+ * @file triangulation.h
+ * @brief Delaunay triangulation algorithms
+ */
+
+#ifndef TRIANGULATION_H
+#define TRIANGULATION_H
+
+#include "core.h"
+
+/*
+ * @struct Circle
+ * @brief additional structure for the circle
+ */
+
+
+
+
+
+
+
+
+
+#endif
+ 
+
